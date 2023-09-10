a00fface15cdbae2fb9c1feef5d0319e26999363
Martina Vangelova <mvangelovaa@abv.bg>
2023-09-10 09:50:28 +0300

    set up tailwind config

--- lemonade-app/tailwind.config.js
+++ lemonade-app/tailwind.config.js
@@ -3,11 +3,14 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    './src/components/**/*.{js,ts,jsx,tsx,}',
   ],
   theme: {
-    extend: {},
+    
+    extend: {
+  
+    
   },
+  
   plugins: [],
 }
-
+}
