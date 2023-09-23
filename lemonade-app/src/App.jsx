4e6c61a4c4eeaaaa134d4674deaed0a4e5431595
Martina Vangelova <mvangelovaa@abv.bg>
2023-09-23 14:16:20 +0300

    add pictures for menu

--- lemonade-app/src/App.jsx
+++ lemonade-app/src/App.jsx
@@ -3,12 +3,13 @@ import Footer from "./components/Footer";
 
 import Header from "./components/Header";
 import Home from "./components/Home";
-
+import Menu from "./components/Menu";
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Menu />
       <Footer />
     </div>
   );
