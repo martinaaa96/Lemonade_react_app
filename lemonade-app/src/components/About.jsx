32346b81a2476cce30f0ddd8b0977b752ff959ed
Martina Vangelova <mvangelovaa@abv.bg>
2023-10-01 16:10:41 +0300

    about page

--- lemonade-app/src/components/About.jsx
+++ lemonade-app/src/components/About.jsx
@@ -2,23 +2,37 @@ import homemade from "/homemade.jpg";
 import smoothie from "/smoothie.jpg";
 export default function About() {
   return (
-    /* / gfg/*/ <div className="container mx-auto p-4 flex">
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="p-4 rounded-lg">
-          <img src={homemade} alt="homemade" className="w-full h-auto" />
-        </div>
-
-        <div className=" p-4 rounded-lg">
-          <img src={smoothie} alt="smoothie" className="w-full h-auto" />
-        </div>
-        <div className="bg-pink-200 p-4 rounded-lg relative md:h-1/3 object-center">
-          <p className="text-lg font-semibold">Text Content</p>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            We didnt reinvent the wheel
+          </h2>
+          <p className="mb-4">
+            We are strategists, designers and developers. Innovators and problem
+            solvers. Small enough to be simple and quick, but big enough to
+            deliver the scope you want at the pace you need. Small enough to be
+            simple and quick, but big enough to deliver the scope you want at
+            the pace you need.
+          </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
-            metus eget purus ultricies vehicula.
+            We are strategists, designers and developers. Innovators and problem
+            solvers. Small enough to be simple and quick.
           </p>
         </div>
+        <div className="grid grid-cols-2 gap-4 mt-8">
+          <img
+            className="w-full rounded-lg"
+            src={homemade}
+            alt="office content 1"
+          />
+          <img
+            className="mt-4 w-full lg:mt-10 rounded-lg"
+            src={smoothie}
+            alt="office content 2"
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
