b316468ec3eb9bebe60d3f40acb2559bac85bcbc
Martina Vangelova <mvangelovaa@abv.bg>
2023-10-01 17:03:39 +0300

    add more products

--- lemonade-app/src/data.js
+++ lemonade-app/src/data.js
@@ -4,7 +4,7 @@ export const data = [
         id: 1,
         name: 'Blueberry',
         category: 'lemonade',
-        description:'',
+        description: '',
         image: '/lemonade-app/public/blueberry.jpg',
         price: '10',
 
@@ -13,7 +13,7 @@ export const data = [
         id: 2,
         name: 'Strawberry',
         category: 'lemonade',
-        description:'',
+        description: '',
         image: '/lemonade-app/public/strawberry.jpg',
         price: '12',
 
@@ -23,7 +23,7 @@ export const data = [
         id: 3,
         name: 'Watermelon',
         category: 'lemonade',
-        description:'',
+        description: '',
         image: '/lemonade-app/public/watermelon.jpg',
         price: '13',
 
@@ -32,7 +32,7 @@ export const data = [
         id: 4,
         name: 'Lemonade',
         category: 'lemonade',
-        description:'',
+        description: '',
         image: '/lemonade-app/public/lemonade.jpg',
         price: '8',
 
@@ -41,7 +41,7 @@ export const data = [
         id: 5,
         name: 'Blueberry',
         category: 'lemonade',
-        description:'',
+        description: '',
         image: '/lemonade-app/public/blueberry.jpg',
         price: '10',
 
@@ -50,7 +50,7 @@ export const data = [
         id: 6,
         name: 'Strawberry',
         category: 'lemonade',
-        description:'',
+        description: '',
         image: '/lemonade-app/public/strawberry.jpg',
         price: '10',
 
@@ -58,57 +58,57 @@ export const data = [
     },
     {
         id: 7,
-        name: 'Blueberry',
-        category: 'smothie',
-        description:'',
+        name: 'Banana',
+        category: 'smoothie',
+        description: '',
         image: '/lemonade-app/public/blueberry.jpg',
         price: '10',
 
     },
     {
         id: 8,
-        name: 'Blueberry',
-        category: 'lemonade',
-        description:'',
-        image: '/lemonade-app/public/blueberry.jpg',
+        name: 'Forest fruits',
+        category: 'smoothie',
+        description: '',
+        image: '/lemonade-app/public/forest_frozen-fruit-smoothie.jpg',
         price: '10',
 
     },
     {
         id: 9,
         name: 'Blueberry',
-        category: 'lemonade',
-        description:'',
-        image: '/lemonade-app/public/blueberry.jpg',
+        category: 'smoothie',
+        description: '',
+        image: '/lemonade-app/public/blueberry-smoothie-5.jpg',
         price: '10',
 
     },
     {
         id: 10,
-        name: 'Blueberry',
-        category: 'lemonade',
-        description:'',
-        image: '/lemonade-app/public/blueberry.jpg',
+        name: 'Strawberry banana',
+        category: 'smoothie',
+        description: '',
+        image: '/lemonade-app/public/strawberry-banana-smoothie.jpeg',
         price: '10',
 
 
     },
     {
         id: 11,
-        name: 'Blueberry',
-        category: 'lemonade',
-        description:'',
-        image: '/lemonade-app/public/blueberry.jpg',
+        name: 'Forest fruits',
+        category: 'smoothie',
+        description: '',
+        image: '/lemonade-app/public/forest_frozen-fruit-smoothie.jpg',
         price: '10',
 
 
     },
     {
         id: 12,
-        nname: 'Blueberry',
-        category: 'lemonade',
-        description:'',
-        image: '/lemonade-app/public/blueberry.jpg',
+        nname: 'Banana',
+        category: 'smoothie',
+        description: '',
+        image: '/lemonade-app/public/Peanut-Butter-Banana-Smoothie-recipe.webp',
         price: '10',
 
     }
