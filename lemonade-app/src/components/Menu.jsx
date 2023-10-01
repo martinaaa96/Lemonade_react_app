1f8d9679c0e4214a235d3eccad55bd0c153fb5b9
Martina Vangelova <mvangelovaa@abv.bg>
2023-10-01 16:58:17 +0300

    update menu page

--- lemonade-app/src/components/Menu.jsx
+++ lemonade-app/src/components/Menu.jsx
@@ -15,10 +15,12 @@ export default function Menu() {
           <div className="bg-white rounded-lg shadow-md p-4">
             <img
               src={blueberry}
+             
               alt="Product 1"
               className="w-full  object-cover mb-2 hover:scale-110 duration-500"
             />
             <h2 className="text-lg font-semibold">Product 1</h2>
+            <p className="text-gray-600">Category: </p>
             <p className="text-gray-600">Description of Product 1.</p>
             <p className="text-blue-500 font-semibold mt-2">$19.99</p>
           </div>
@@ -29,6 +31,7 @@ export default function Menu() {
               className="w-full  object-cover mb-2 hover:scale-110 duration-500"
             />
             <h2 className="text-lg font-semibold">Product 1</h2>
+            <p className="text-gray-600">Category: </p>
             <p className="text-gray-600">Description of Product 1.</p>
             <p className="text-blue-500 font-semibold mt-2">$19.99</p>
           </div>
@@ -39,6 +42,7 @@ export default function Menu() {
               className="w-full  object-cover mb-2 hover:scale-110 duration-500"
             />
             <h2 className="text-lg font-semibold">Product 1</h2>
+            <p className="text-gray-600">Category: </p>
             <p className="text-gray-600">Description of Product 1.</p>
             <p className="text-blue-500 font-semibold mt-2">$19.99</p>
           </div>
@@ -49,6 +53,7 @@ export default function Menu() {
               className="w-full  object-cover mb-2 hover:scale-110 duration-500"
             />
             <h2 className="text-lg font-semibold">Product 1</h2>
+            <p className="text-gray-600">Category: </p>
             <p className="text-gray-600">Description of Product 1.</p>
             <p className="text-blue-500 font-semibold mt-2">$19.99</p>
           </div>
