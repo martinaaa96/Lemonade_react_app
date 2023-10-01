4bd972e0f0c919d54577a927c1fd98030bf8f01c
Martina Vangelova <mvangelovaa@abv.bg>
2023-10-01 16:58:25 +0300

    add data produucts

---
+++ lemonade-app/src/data.js
@@ -0,0 +1,115 @@
+
+export const data = [
+    {
+        id: 1,
+        name: 'Blueberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+    },
+    {
+        id: 2,
+        name: 'Strawberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/strawberry.jpg',
+        price: '12',
+
+
+    },
+    {
+        id: 3,
+        name: 'Watermelon',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/watermelon.jpg',
+        price: '13',
+
+    },
+    {
+        id: 4,
+        name: 'Lemonade',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/lemonade.jpg',
+        price: '8',
+
+    },
+    {
+        id: 5,
+        name: 'Blueberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+    },
+    {
+        id: 6,
+        name: 'Strawberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/strawberry.jpg',
+        price: '10',
+
+
+    },
+    {
+        id: 7,
+        name: 'Blueberry',
+        category: 'smothie',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+    },
+    {
+        id: 8,
+        name: 'Blueberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+    },
+    {
+        id: 9,
+        name: 'Blueberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+    },
+    {
+        id: 10,
+        name: 'Blueberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+
+    },
+    {
+        id: 11,
+        name: 'Blueberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+
+    },
+    {
+        id: 12,
+        nname: 'Blueberry',
+        category: 'lemonade',
+        description:'',
+        image: '/lemonade-app/public/blueberry.jpg',
+        price: '10',
+
+    }
+]
