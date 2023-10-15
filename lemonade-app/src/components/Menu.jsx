b48db61d3c3a75b5e0c6e5411d314e8c3f3f177b
Martina Vangelova <mvangelovaa@abv.bg>
2023-10-15 16:12:38 +0300

    update text style

--- lemonade-app/src/components/Menu.jsx
+++ lemonade-app/src/components/Menu.jsx
@@ -6,12 +6,28 @@ import lemonade from "/lemonade.jpg";
 export default function Menu() {
   return (
     <>
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-semibold mb-4 text-center">
-          Our Products
-        </h1>
+      <div className="container mx-auto p-4 font-serif">
+        <div className="mb-4 text-center">
+          <h1 className="text-2xl font-semibold ">Our Products</h1>
+        </div>
+        <div className="flex justify-end items-center mb-4">
+          <div className="flex items-center">
+            <label htmlFor="sort-select" className="mr-2">
+              Sort By:
+            </label>
+            <select
+              id="sort-select"
+              className="text-black border border-black "
+            >
+              <option value="price-asc">Price Low to High</option>
+              <option value="price-desc">Price High to Low</option>
+              <option value="name-asc">A to Z</option>
+              <option value="name-desc">Z to A</option>
+            </select>
+          </div>
+        </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-5">
           <div className="bg-white rounded-lg shadow-md p-4">
             <img
               src={blueberry}
@@ -56,7 +72,11 @@ export default function Menu() {
             <p className="text-gray-600">Ingredients: </p>
             <p className="text-blue-500 font-semibold mt-2">Price: 19.99</p>
           </div>
-          <button className="text-center">Load</button>
+        </div>
+        <div className="flex justify-center font-serif ">
+          <button className="bg-yellow-500 hover:text-black duration-500 text-white my-7 py-2 px-4 rounded-full ">
+            Load
+          </button>
         </div>
       </div>
     </>
