3575f8a262006319c72f8298aa39a38e39e4bead
Martina Vangelova <mvangelovaa@abv.bg>
2023-10-15 15:16:33 +0300

    update About

--- lemonade-app/src/components/About.jsx
+++ lemonade-app/src/components/About.jsx
@@ -6,18 +6,20 @@ export default function About() {
       <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            We didnt reinvent the wheel
+            Welcome to Homemade Lemonade!
           </h2>
           <p className="mb-4">
-            We are strategists, designers and developers. Innovators and problem
-            solvers. Small enough to be simple and quick, but big enough to
-            deliver the scope you want at the pace you need. Small enough to be
-            simple and quick, but big enough to deliver the scope you want at
-            the pace you need.
+            Welcome to Homemade Lemonade, where refreshing lemonade meets the
+            warmth of homemade goodness. We are passionate about crafting the
+            most delightful, thirst-quenching lemonade, just the way you
+            remember it from your childhood. Our story began with a simple idea:
+            to share the joy of sipping on a cool glass of lemonade made with
+            love and the finest ingredients. We believe that life is best
+            moments are often shared over a glass of something sweet.
           </p>
-          <p>
-            We are strategists, designers and developers. Innovators and problem
-            solvers. Small enough to be simple and quick.
+          <p className="font-semibold">
+            We are thrilled to announce the latest addition to our menu –
+            freshly blended smoothies!
           </p>
         </div>
         <div className="grid grid-cols-2 gap-4 mt-8">
