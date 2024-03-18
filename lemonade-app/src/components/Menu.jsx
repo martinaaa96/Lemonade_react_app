bde828a4b4c58c2a30bacf17914389a9014ac164
Martina Vangelova <mvangelovaa@abv.bg>
2024-03-18 15:15:06 +0200

    update

--- lemonade-app/src/components/Menu.jsx
+++ lemonade-app/src/components/Menu.jsx
@@ -2,11 +2,10 @@
 //import watermelon from "/watermelon.jpg";
 //import blueberry from "/blueberry.jpg";
 //import lemonade from "/lemonade.jpg";
-import { data } from "../data";
+import { data } from "../utils/data";
 
 export default function Menu() {
   const products = data;
-  console.log(products.name);
 
   return (
     <>
@@ -30,25 +29,26 @@ export default function Menu() {
             </select>
           </div>
         </div>
-        {products.map((product) => {
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-5">
-            <div className="bg-white rounded-lg shadow-md p-4">
-              <img
-                src={products.image}
-                alt={`${products.id}`}
-                className="w-full max-w-xs object-cover mb-2 hover:scale-110 duration-500"
-              />
-              <h2 className="text-lg font-semibold">{products.name}</h2>
-              <p className="text-gray-600">Category: {products.category}</p>
-              <p className="text-gray-600">
-                Ingredients: {product.ingredients}
-              </p>
-              <p className="text-blue-500 font-semibold mt-2">
-                Price: {products.price}
-              </p>
-            </div>
-          </div>;
-        })}
+        {products &&
+          products.map((product) => {
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-5">
+              <div className="bg-white rounded-lg shadow-md p-4">
+                <img
+                  src={product.image}
+                  alt={product.id}
+                  className="w-full max-w-xs object-cover mb-2 hover:scale-110 duration-500"
+                />
+                <h2 className="text-lg font-semibold">{product.name}</h2>
+                <p className="text-gray-600">Category: {product.category}</p>
+                <p className="text-gray-600">
+                  Ingredients: {product.ingredients}
+                </p>
+                <p className="text-blue-500 font-semibold mt-2">
+                  Price: {product.price}
+                </p>
+              </div>
+            </div>;
+          })}
 
         <div className="flex justify-center font-serif ">
           <button className="bg-yellow-500 hover:text-black duration-500 text-white my-7 py-2 px-4 rounded-full ">
