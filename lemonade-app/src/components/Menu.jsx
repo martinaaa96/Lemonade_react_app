d08b6999f8b7be2d2f1a5d8973e3c37b1136bedb
Martina Vangelova <mvangelovaa@abv.bg>
2024-03-27 13:17:41 +0200

    add products

--- lemonade-app/src/components/Menu.jsx
+++ lemonade-app/src/components/Menu.jsx
@@ -1,11 +1,6 @@
-//import strawberry from "/strawberry.jpg";
-//import watermelon from "/watermelon.jpg";
-//import blueberry from "/blueberry.jpg";
-//import lemonade from "/lemonade.jpg";
 import { data } from "../utils/data";
 
 export default function Menu() {
-const products = data
   return (
     <>
       <div className="container mx-auto p-4 font-serif">
@@ -28,12 +23,11 @@ const products = data
             </select>
           </div>
         </div>
-        {products.map((product) => {
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-5">
-            <div className="bg-white rounded-lg shadow-md p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-5">
+          {data.map((product) => (
+            <div key={product.id} className="bg-white rounded-lg shadow-md p-4">
               <img
                 src={product.image}
-                alt={product.id}
                 className="w-full max-w-xs object-cover mb-2 hover:scale-110 duration-500"
               />
               <h2 className="text-lg font-semibold">{product.name}</h2>
@@ -45,8 +39,8 @@ const products = data
                 Price: {product.price}
               </p>
             </div>
-          </div>;
-        })}
+          ))}
+        </div>
 
         <div className="flex justify-center font-serif ">
           <button className="bg-yellow-500 hover:text-black duration-500 text-white my-7 py-2 px-4 rounded-full ">
