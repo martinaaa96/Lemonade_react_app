e834648bb855847aece1f7af7d6ac7c8e94a84d2
Martina Vangelova <mvangelovaa@abv.bg>
2024-04-05 12:26:02 +0300

    update footer css

--- lemonade-app/src/components/Footer.jsx
+++ lemonade-app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <>
       <footer className="bg-white dark:bg-gray-900 font-serif">
-        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="mx-auto m:full max-w-screen-xl p-4 py-8 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <Link className="flex items-center">
@@ -45,12 +45,11 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <hr className="my-1 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-3" />
           <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 ">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2023
-              <a className="hover:underline"> Homemade Lemonade</a>. All Rights
-              Reserved.
+              <a className="hover:underline"> Homemade Lemonade</a>. All Rights Reserved.
             </span>
             <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
               <SiFacebook className="text-gray-500 hover:text-gray-900 dark:hover:text-white hover:scale-110 duration-500" />
