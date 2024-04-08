d39cf72407f9a1bce06014ab50bdb276937d4ac2
Martina Vangelova <mvangelovaa@abv.bg>
2024-04-08 17:06:59 +0300

    create CatalogItem

---
+++ lemonade-app/src/components/CatalogItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function CatalogItem({ product }) {
+  return (
+    <div key={product.id} className="bg-white rounded-lg shadow-md p-4">
+      <img
+        src={product.image}
+        alt={product.name}
+        className="w-full max-w-xs object-cover mb-2 hover:scale-110 duration-500"
+      />
+      <h2 className="text-lg font-semibold">{product.name}</h2>
+      <p className="text-gray-600">Category:{product.category}</p>
+      <p className="text-gray-600">Ingredients:{product.ingredients}</p>
+      <p className="text-blue-500 font-semibold mt-2">
+        Price: {product.price} $
+      </p>
+    </div>
+  );
+}
