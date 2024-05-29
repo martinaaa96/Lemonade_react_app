d07870e7b5caf18d4e41ba45b737afdac9b0e59a
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-29 17:51:15 +0300

    update CatalogItem

--- lemonade-app/src/components/CatalogItem.jsx
+++ lemonade-app/src/components/CatalogItem.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ProductContext } from "../context/productContext";
+import { ProductContext } from "../context/ProductContext";
 
 export default function CatalogItem({ product }) {
   const { addToCart } = useContext(ProductContext);
