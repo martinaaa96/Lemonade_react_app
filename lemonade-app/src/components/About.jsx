264ff0fa27bd14f2ad90da0c1deec5b6ed6e7f4f
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-31 19:38:46 +0300

    fixed responsive for about page

--- lemonade-app/src/components/About.jsx
+++ lemonade-app/src/components/About.jsx
@@ -3,9 +3,9 @@ import smoothie from "/smoothie.jpg";
 export default function About() {
   return (
     <section className="bg-white dark:bg-gray-900 font-serif">
-      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div className="gap-16 items-center py-20 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          <h2 className="mb-6 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             Welcome to Homemade Lemonade!
           </h2>
           <p className="mb-4">
